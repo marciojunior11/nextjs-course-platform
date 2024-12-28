b41a969f509b548e53aef3b2e89ecfdcdf0a6de0
marciojunior11 <marcio_ale1@hotmail.com>
2024-12-28 14:21:42 -0300

    feat: styles in header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { MdOpenInNew } from "react-icons/md";
 
 export const Header = () => {
+    const currentPath = usePathname();
+
     return (
-        <nav>
-            <ul>
+        <nav className="flex items-center justify-center bg-primary py-4">
+            <ul className="flex gap-4 items-center my-2">
                 <li>
-                    <Link href="/">Página inicial</Link>
+                    <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
+                        CODARSE
+                    </Link>
                 </li>
+
                 <li>
-                    <Link href="/cursos">Página inicial</Link>
+                    <Link href="/" data-active={currentPath === "/"} className="data-[active=true]:underline">
+                        Página inicial
+                    </Link>
                 </li>
+
                 <li>
-                    <Link href="https://blog.codarse.com" target="_blank">Blog</Link>
+                    <Link href="/cursos" data-active={currentPath === "/cursos"} className="data-[active=true]:underline">
+                        Cursos
+                    </Link>
+                </li>
+                
+                <li>
+                    <Link href="https://blog.codarse.com" target="_blank" className="flex gap-1 items-center">
+                        Blog
+                        <MdOpenInNew />
+                    </Link>
                 </li>
             </ul>
         </nav>
