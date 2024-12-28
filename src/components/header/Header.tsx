21682a4bf045fd8342f4d92d93ce85de80386658
marciojunior11 <marcio_ale1@hotmail.com>
2024-12-28 15:01:34 -0300

    feat: config responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,17 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOpenInNew } from "react-icons/md";
+import { MdMenu, MdOpenInNew } from "react-icons/md";
 
 export const Header = () => {
     const currentPath = usePathname();
 
     return (
-        <nav className="flex items-center justify-center bg-primary py-4">
+        <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+            <button className="sm:hidden">
+                <MdMenu size={24}/>
+            </button>
+
             <ul className="flex gap-4 items-center my-2">
                 <li>
                     <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
@@ -16,25 +20,27 @@ export const Header = () => {
                     </Link>
                 </li>
 
-                <li>
+                <li className="hidden sm:block">
                     <Link href="/" data-active={currentPath === "/"} className="data-[active=true]:underline">
                         Página inicial
                     </Link>
                 </li>
 
-                <li>
+                <li className="hidden sm:block">
                     <Link href="/cursos" data-active={currentPath === "/cursos"} className="data-[active=true]:underline">
                         Cursos
                     </Link>
                 </li>
                 
-                <li>
+                <li className="hidden sm:block">
                     <Link href="https://blog.codarse.com" target="_blank" className="flex gap-1 items-center">
                         Blog
                         <MdOpenInNew />
                     </Link>
                 </li>
             </ul>
+
+            <h1 className="sm:hidden">CodarSe - Página inicial</h1>
         </nav>
     );
 };
