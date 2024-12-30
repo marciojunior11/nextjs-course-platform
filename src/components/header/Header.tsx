0bddb55d2dabe67308a04ad6d294632df9cb2975
marciojunior11 <marcio_ale1@hotmail.com>
2024-12-30 12:11:16 -0300

    feat: add navigation in header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,20 +6,22 @@ import { useEffect, useState } from "react";
 import { MdMenu, MdOpenInNew } from "react-icons/md";
 
 export const Header = () => {
+    const [drawer, setDrawer] = useState(false);
     const [title, setTitle] = useState("CodarSe");
     const currentPath = usePathname();
 
     useEffect(() => {
         setTitle(document.title);
+        setDrawer(false);
     }, [currentPath]);
 
     return (
         <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
-            <button className="sm:hidden">
-                <MdMenu size={24}/>
+            <button className="sm:hidden" onClick={() => setDrawer(true)}>
+                <MdMenu size={24} />
             </button>
 
-            <ul className="flex gap-4 items-center my-2">
+            <ul className="flex gap-4 items-center my-2" tabIndex={drawer ? -1 : undefined}>
                 <li>
                     <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
                         CODARSE
@@ -37,7 +39,7 @@ export const Header = () => {
                         Cursos
                     </Link>
                 </li>
-                
+
                 <li className="hidden sm:block">
                     <Link href="https://blog.codarse.com" target="_blank" className="flex gap-1 items-center">
                         Blog
@@ -45,6 +47,35 @@ export const Header = () => {
                     </Link>
                 </li>
             </ul>
+
+            <div
+                data-open={drawer}
+                tabIndex={drawer ? undefined : -1}
+                onClick={() => setDrawer(false)}
+                className="fixed top-0 right-0 bottom-0 left-0 bg-gradient-to-r from-background transition-transform data-[open=false]:-translate-x-full"
+            >
+                <ul onClick={e => e.stopPropagation()} className="flex flex-col w-60 h-full gap-4 p-4 bg-background">
+                    <li className="">
+                        <Link href="/" data-active={currentPath === "/"} className="data-[active=true]:underline">
+                            Página inicial
+                        </Link>
+                    </li>
+
+                    <li className="">
+                        <Link href="/cursos" data-active={currentPath === "/cursos"} className="data-[active=true]:underline">
+                            Cursos
+                        </Link>
+                    </li>
+
+                    <li className="">
+                        <Link href="https://blog.codarse.com" target="_blank" className="flex gap-1 items-center">
+                            Blog
+                            <MdOpenInNew />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
 
             <h1 className="sm:hidden">
                 {title}
