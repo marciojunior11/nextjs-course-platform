7d6ca4f632902acbb12293c7645df5555369c638
marciojunior11 <marcio_ale1@hotmail.com>
2025-01-03 17:38:43 -0300

    feat: add left and right scroll button to section component

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { Card, ICardProps } from "../card/Card";
+import { useRef } from "react";
 
 interface ISectionProps {
     title: string;
@@ -7,11 +11,23 @@ interface ISectionProps {
 }
 
 export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
+    const scrollRef = useRef<HTMLUListElement>(null);
+
+    const handleScroll = (scroll: number) => {
+        const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+
+        scrollRef.current?.scrollTo({
+            behavior: "smooth",
+            left: currentScrollLeft + scroll
+        });
+    };
+
     return (
         <section className="flex flex-col gap-4 px-4">
             <h2 className="font-bold text-xl ">{title}</h2>
 
             <ul
+                ref={scrollRef}
                 data-variant={variant}
                 className={`
                     grid gap-2 grid-cols-1 sm:grid-cols-none
@@ -21,6 +37,13 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
                     data-[variant=h-list]:sm:overflow-x-auto
                 `}
             >
+                <button 
+                    onClick={() => handleScroll(-350)}
+                    className="h-14 w-14 bg-primary rounded-full flex justify-center items-center sticky my-auto left-0 -ml-14"
+                >
+                    <MdKeyboardArrowLeft size={32} />
+                </button>
+
                 {items.map(item => (
                     <li key={item.title} data-variant={variant} className="w-full data-[variant=h-list]:sm:w-72">
                         <Card
@@ -31,6 +54,13 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
                         />
                     </li>
                 ))}
+
+                <button
+                    onClick={() => handleScroll(350)}
+                    className="h-14 w-14 bg-primary rounded-full flex justify-center items-center sticky my-auto right-0 -ml-14"
+                >
+                    <MdKeyboardArrowRight size={32} />
+                </button>
             </ul>
         </section>
     );
